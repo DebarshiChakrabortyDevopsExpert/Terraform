7bed55bd065555f82490a09a4ea1a9c5071d7a17
DebarshiChakrabortyDevopsExpert <166385395+DebarshiChakrabortyDevopsExpert@users.noreply.github.com>
2024-04-08 17:04:08 +0530

    Update logicapp.tf

--- modules/az-logicapp/logicapp.tf
+++ modules/az-logicapp/logicapp.tf
@@ -1,5 +1,5 @@
 resource "azurerm_logic_app_workflow" "logic_app_workflow" {
-  name                               = "lgapp-rms-${var.logic_app_workflow.env}-uks-${var.logic_app_workflow.instance_number}"
+  name                               = "lgapp-org-${var.logic_app_workflow.env}-uks-${var.logic_app_workflow.instance_number}"
   location                           = var.logic_app_workflow.location
   resource_group_name                = var.logic_app_workflow.resource_group_name
   integration_service_environment_id = try(var.logic_app_workflow.integration_service_environment_id, null)
